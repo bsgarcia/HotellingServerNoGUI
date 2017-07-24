5bee8751e553ed84a071d4bc79a53e5b34ed1015
AurelienNioche <nioche.aurelien@gmail.com>
2017-07-24 13:00:04 +0200

    Small debug.

--- main_bot_server.py
+++ main_bot_server.py
@@ -1,6 +1,11 @@
 from bots.bot_server import main
-
+import sys
 
 if __name__ == "__main__":
 
-    main("firm")
+    if len(sys.argv) > 0:
+        type_of_player = sys.argv[1]
+    else:
+        raise Exception("You should indicate a type of player such as 'customer' or 'firm'.")
+
+    main(type_of_player)
